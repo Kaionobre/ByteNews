f8d6c4928b8cbf94550b26424d673b7648b6e8be
kaionobre <kaionobrega00@gmail.com>
2024-09-15 15:21:50 -0300

    ATT: Atualizando a função para pegar a notícia e o link

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from scrapy import pegar_noticias
+from scrapy import pegar_noticias_e_links
 import json
 
 app = FastAPI()
 
 @app.get("/noticias")
 async def mostrar_noticias():
-    noticias = pegar_noticias()
-    print(noticias)
+    noticias = pegar_noticias_e_links()
     return {"dados": noticias}
--- scrapy.py
+++ scrapy.py
@@ -1,19 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def pegar_noticias():
+def pegar_noticias_e_links():
     url = 'https://www.adrenaline.com.br/noticias/'                    
     response = requests.get(url)
     raw_html = response.text
     parsed_html = BeautifulSoup(raw_html, 'html.parser')
 
-    article = parsed_html.find('div', class_='archive-list-posts')
-    noticias = article.find_all('h2')
+    artigo = parsed_html.find('div', class_='archive-list-posts')
+    artigos = artigo.find_all('article', class_='feed feed-4-list')
 
     lista_noticias = []
 
-    for noticia in noticias:
+    for artigo in artigos:
+        titulo_elemento = artigo.find('h2')
+        link_elemento = artigo.find('a')
+        
+        if titulo_elemento and link_elemento:
+            lista_noticias.append({
+                "titulo": titulo_elemento.text.strip(),
+                "link": link_elemento['href']
+            })
 
-        lista_noticias.append({"titulo": noticia.text})
-    
     return lista_noticias
+
+
